a52c66296b0efe47465ee0311dc7f9e6d400e4db
vedXpatel <ved.gp@somaiya.edu>
2023-08-03 02:33:01 +0530

    :mega: modularization

---
+++ components/GrandCalculations.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {DataTable} from "react-native-paper";
+import {Text} from "react-native";
+
+export const GrandCalculations = (props) => {
+    const {grandCFT, grandPrice} = props;
+    return (
+        <DataTable
+            style={{
+                position: "relative",
+                left: -width / 5,
+                width: width + width / 5,
+            }}
+        >
+            <DataTable.Row
+                style={{
+                    borderBottomWidth: 1,
+                    borderColor: "black",
+                    overflow: "hidden",
+                }}
+            >
+                <DataTable.Cell
+                    textStyle={{
+                        fontSize: 17,
+                        fontWeight: "bold",
+                        left: width / 3.3,
+                        width: width / 2,
+                    }}
+                    numeric
+                >
+                    <Text>Grand CFT </Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                    textStyle={{ fontSize: 17, fontWeight: "bold" }}
+                    numeric
+                >
+                    <Text>:</Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                    textStyle={{ fontSize: 17, fontWeight: "bold" }}
+                    numeric
+                >
+                    <Text>{grandCFT.toFixed(4)}</Text>
+                </DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row
+                style={{
+                    borderBottomWidth: 1,
+                    borderBottomColor: "black",
+                    borderColor: "black",
+                    overflow: "hidden",
+                }}
+            >
+                <DataTable.Cell
+                    textStyle={{
+                        fontSize: 17,
+                        fontWeight: "bold",
+                        left: width / 3.3,
+                        width: width / 2,
+                    }}
+                    numeric
+                >
+                    <Text>Grand Price</Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                    textStyle={{ fontSize: 17, fontWeight: "bold" }}
+                    numeric
+                >
+                    <Text>:</Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                    textStyle={{ fontSize: 17, fontWeight: "bold" }}
+                    numeric
+                >
+                    <Text>{grandPrice.toFixed(2)}</Text>
+                </DataTable.Cell>
+            </DataTable.Row>
+        </DataTable>
+    )
+}
